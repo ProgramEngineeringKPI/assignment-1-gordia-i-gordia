6629cfad229592c00353a87a424e4a16545baa37
NikitaGordia <nikita.gordia@gmail.com>
2018-04-04 10:48:40 +0300

    Initial Commit

---
+++ prog_lab/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  prog_lab
+//
+//  Created by Nikita Gordia on 4/4/18.
+//  Copyright © 2018 Nikita Gordia. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
